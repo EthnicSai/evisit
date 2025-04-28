edc0c6d94b8d89ee6b1a7103f33fd2b9fef8e663
EthnicSai <ethnicsai.04@gmail.com>
2025-04-28 22:58:24 +0530

    error resolved

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/SignUpPage.css';
+import '../styles/SignupPage.css';
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
